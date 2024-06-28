85d65a973d3d19478501cd7a0331328047d37fd3
Sumit Maurya <mauryasumit222@gmail.com>
2024-06-28 18:14:20 +0530

    [ 28 June ] ["Fixing the code in ExpireMaintenanceBillJob"]

--- app/jobs/expire_maintenance_bill_job.rb
+++ app/jobs/expire_maintenance_bill_job.rb
@@ -5,7 +5,7 @@ class ExpireMaintenanceBillJob < ApplicationJob
     maintenance_bill = MaintenanceBill.find_by(id: maintenance_bill_id)
     return unless maintenance_bill
 
-    if maintenance_bill.status == 'pending' && maintenance_bill.end_date < Date.current
+    if maintenance_bill.status == 'pending' && maintenance_bill.end_date == Date.current
       maintenance_bill.update(status: 'expired')
     end
   end
